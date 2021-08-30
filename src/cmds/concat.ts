ca2d87c6bfaea5dac3f022515bb4ce3831d1ca9b
Scott Leslie <scottpleslie@gmail.com>
2021-08-29 22:45:46 -0400

    Added concat operation

---
+++ src/cmds/concat.ts
@@ -0,0 +1,36 @@
+
+import { MeshInterface } from "../lib/mesh";
+import { STLFile } from "../lib/stlfile";
+
+exports.command = 'concat [infile1] [infile2] [outfile]'
+exports.desc = 'Add two meshes into one file'
+exports.builder = {
+  infile1: {
+    default: '',
+    demandOption: true
+  },
+  infile2: {
+    default: '',
+    demandOption: true
+  },
+  outfile: {
+    default: '',
+    demandOption: true
+  },
+}
+
+exports.handler = function (argv:any) {
+  try {
+    let stlFile : STLFile = new STLFile();
+    let mesh1:MeshInterface = stlFile.readSTLFile(argv.infile1);
+    let mesh2:MeshInterface = stlFile.readSTLFile(argv.infile2);
+
+    mesh1.addMesh(mesh2);
+    
+    stlFile.writeSTLFile(argv.outfile, "Concat_" + argv.infile1 +"_" + argv.infile2, mesh1);
+
+  }
+  catch (e) {
+    console.log("Error: Unable to load file\n", e.message);
+  }
+}
