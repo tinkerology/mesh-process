a4ca4f4635bd965c18dbb4dd912d1ca3013ccc4a
Scott Leslie <scottpleslie@gmail.com>
2021-09-05 11:46:30 -0400

    New

---
+++ src/cmds/filter-vertex-replace.ts
@@ -0,0 +1,59 @@
+
+import { MeshInterface, VertexFilterAbove, VertexFilterReplace, VertexFilterTranslate } from "../lib/mesh";
+import { MeshOperations } from "../lib/mesh";
+import { STLFile } from "../lib/stlfile";
+import { Vertex } from "../lib/vertex";
+
+exports.command = 'filter-vertex-replace [xOrig] [yOrig] [zOrig] [xNew] [yNew] [zNew] [infile] [outfile]'
+exports.desc = 'Change the location of the vertex in the specified STL file'
+exports.builder = {
+  xOrig: {
+    default: Number.MAX_VALUE,
+    description: 'Vertex to move: x'
+  },
+  yOrig: {
+    default: Number.MAX_VALUE,
+    description: 'Vertex to move: y'
+  },
+  zOrig: {
+    default: Number.MAX_VALUE,
+    description: 'Vertex to move: z'
+  },
+  xNew: {
+    default: Number.MAX_VALUE,
+    description: 'New vertex location: x'
+  },
+  yNew: {
+    default: Number.MAX_VALUE,
+    description: 'New vertex location: y'
+  },
+  zNew: {
+    default: Number.MAX_VALUE,
+    description: 'New vertex location: z'
+  },
+  infile: {
+    default: '',
+    demandOption: true
+  },
+  outfile: {
+    default: '',
+    demandOption: true
+  },
+}
+
+exports.handler = function (argv:any) {
+  try {
+    let stlFile : STLFile = new STLFile();
+    let mesh:MeshInterface = stlFile.readSTLFile(argv.infile);
+
+    let translatedMesh:MeshInterface = MeshOperations.filterVertices(mesh,
+          new VertexFilterReplace(new Vertex(argv.xOrig, argv.yOrig, argv.zOrig),
+                                  new Vertex(argv.xNew, argv.yNew, argv.zNew) ) );
+    );
+
+    stlFile.writeSTLFile(argv.outfile, "FilterVertexReplace_x" + argv.x + "_y" + argv.y + "_z" + argv.z, translatedMesh);
+  }
+  catch (e) {
+    console.log("Error: Unable to load file\n", e.message);
+  }
+}
