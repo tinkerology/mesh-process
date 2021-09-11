2d77f9af0f5875022b677c6eb2d36fabbaf5b462
Scott Leslie <scottpleslie@gmail.com>
2021-09-11 00:32:04 -0400

    Changed buildRectangle to return new Mesh.

--- src/lib/meshbuilder.ts
+++ src/lib/meshbuilder.ts
@@ -1,16 +1,19 @@
 
+import { Mesh } from "./mesh";
 import { MeshInterface } from "./mesh";
 import { Triangle } from "./triangle";
 import { VertexInterface } from "./vertex";
 
 export class MeshBuilder {
 
-    static buildRectangle(mesh:MeshInterface, v1:VertexInterface, v2:VertexInterface,
-                          v3:VertexInterface, v4:VertexInterface) {
+    static buildRectangle(v1:VertexInterface, v2:VertexInterface,
+                          v3:VertexInterface, v4:VertexInterface) : MeshInterface {
+        let mesh:MeshInterface = new Mesh();
         let triangle1 = new Triangle(v1,v3,v2);
         mesh.addTriangle(triangle1);
         let triangle2 = new Triangle(v2,v3,v4);
         mesh.addTriangle(triangle2);
+        return mesh;
     }
     
 }
