1a6cc60dc4f205219608dbca70347f6d215df5bb
Scott Leslie <scottpleslie@gmail.com>
2021-10-25 23:37:10 -0400

    test: Changed matrixMultiply to multiply

--- src/lib/__tests__/matrix.test.ts
+++ src/lib/__tests__/matrix.test.ts
@@ -3,10 +3,10 @@
 import { Matrix } from '../matrix';
 
 test('matrixMultiply', () => {
-    let m1:number[][] = Matrix.matrixMultiply([[1,2],[3,4]], [[1,2],[5,6]]);
+    let m1:number[][] = Matrix.multiply([[1,2],[3,4]], [[1,2],[5,6]]);
     expect(m1[0][0]).toBe(11);
 
-    let m2:number[][] = Matrix.matrixMultiply([[1,2,3],[4,5,6]], [[7,8],[9,10],[11,12]]);
+    let m2:number[][] = Matrix.multiply([[1,2,3],[4,5,6]], [[7,8],[9,10],[11,12]]);
     expect(m2[0][0]).toBe(58);
     expect(m2[0][1]).toBe(64);
     expect(m2[1][0]).toBe(139);
