a95492a2b4c89efd19cc838934e8948a3f037734
Scott Leslie <scottpleslie@gmail.com>
2021-12-12 23:41:55 -0500

    test: Improved coverage.

--- src/lib/__tests__/meshinfo.test.ts
+++ src/lib/__tests__/meshinfo.test.ts
@@ -50,12 +50,33 @@ test('getEdgesAtVertex', () => {
     mesh.addTriangle(new Triangle(new Vertex(0,0,0), new Vertex(10,0,0), new Vertex(0,10,10)));
     const edges:Edge[] = MeshInfo.getEdges(mesh);
     expect(edges.length).toBe(6);
+
+    // Test for partial match
     const edgesAtVertex1 = MeshInfo.getEdgesAtVertex(edges, new Vertex(0,0,0));
     expect(edgesAtVertex1.length).toBe(2);
     const connectedEdges:Edge[] = edgesAtVertex1[0];
     expect(connectedEdges.length).toBe(4);
     const disconnectedEdges:Edge[] = edgesAtVertex1[1];
     expect(disconnectedEdges.length).toBe(2);
+
+    // Test for no matches
+    const edgesAtVertex2 = MeshInfo.getEdgesAtVertex(edges, new Vertex(100,100,100));
+    expect(edgesAtVertex2.length).toBe(2);
+    expect(edgesAtVertex2[0].length).toBe(0);
+    expect(edgesAtVertex2[1].length).toBe(6);
+});
+
+test('getEdgesConnectedTo', () => {
+    const mesh:MeshInterface = new Mesh();
+    mesh.addTriangle(new Triangle(new Vertex(0,0,0), new Vertex(10,0,0), new Vertex(0,10,0)));
+    mesh.addTriangle(new Triangle(new Vertex(0,0,0), new Vertex(10,0,0), new Vertex(0,10,10)));
+    mesh.addTriangle(new Triangle(new Vertex(100,100,100), new Vertex(110,100,100), new Vertex(100,110,110)));
+    const edges:Edge[] = MeshInfo.getEdges(mesh);
+    expect(edges.length).toBe(9);
+    const connectedEdges1 = MeshInfo.getEdgesConnectedTo([new Edge(new Vertex(0,0,0), new Vertex(10,0,0))], edges);
+    expect(connectedEdges1.length).toBe(4);
+    const connectedEdges2 = MeshInfo.getEdgesConnectedTo([new Edge(new Vertex(100,100,100), new Vertex(11,0,0))], edges);
+    expect(connectedEdges2.length).toBe(2);
 });
 
 test('getEdgeLengths', () => {
