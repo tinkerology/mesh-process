156d7e2227e2a4b976b69b618a687d5ccadc716b
Scott Leslie <scottpleslie@gmail.com>
2022-02-16 00:25:47 -0500

    test: test saveMesh

--- src/lib/__tests__/meshloader.test.ts
+++ src/lib/__tests__/meshloader.test.ts
@@ -19,3 +19,9 @@ test('loadMesh_missing', () => {
         // Expected
     }
 });
+
+test('saveMesh', () => {
+    const mesh: MeshInterface = MeshLoader.loadMesh('samples/cube.stl');
+    expect(mesh.triangles.length).toBeGreaterThan(0);
+    MeshLoader.saveMesh(mesh, 'unit_test', 'samples/cube_unittest.stl');
+});
