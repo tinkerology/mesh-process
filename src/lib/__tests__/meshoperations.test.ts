b9285f287af184c00cad64c343aaf777d1be24c5
Scott Leslie <scottpleslie@gmail.com>
2022-02-16 00:57:31 -0500

    test: prettier

--- src/lib/__tests__/meshoperations.test.ts
+++ src/lib/__tests__/meshoperations.test.ts
@@ -6,25 +6,29 @@ import { Vertex } from '../vertex';
 
 test('removeTriangles', () => {
     const mesh = MeshBuilder.buildRectangle(
-        new Vertex(0,0,0),
-        new Vertex(10,0,0),
-        new Vertex(10,10,0),
-        new Vertex(0,10,0) );
+        new Vertex(0, 0, 0),
+        new Vertex(10, 0, 0),
+        new Vertex(10, 10, 0),
+        new Vertex(0, 10, 0)
+    );
     expect(mesh.triangles.length).toBe(2);
-    const mesh2 = MeshOperations.removeTriangles(mesh, [new Triangle(
-        new Vertex(0,0,0),
-        new Vertex(10,0,0),
-        new Vertex(10,10,0)
-    )]);
+    const mesh2 = MeshOperations.removeTriangles(mesh, [
+        new Triangle(
+            new Vertex(0, 0, 0),
+            new Vertex(10, 0, 0),
+            new Vertex(10, 10, 0)
+        ),
+    ]);
     expect(mesh2.triangles.length).toBe(1);
 });
 
 test('rotate', () => {
     const mesh = MeshBuilder.buildRectangle(
-        new Vertex(0,0,0),
-        new Vertex(10,0,0),
-        new Vertex(10,10,0),
-        new Vertex(0,10,0) );
+        new Vertex(0, 0, 0),
+        new Vertex(10, 0, 0),
+        new Vertex(10, 10, 0),
+        new Vertex(0, 10, 0)
+    );
     expect(mesh.triangles.length).toBe(2);
     const mesh2 = MeshOperations.rotate(mesh, 0, 0, 0);
     expect(mesh2.triangles.length).toBe(2);
@@ -34,12 +38,16 @@ test('rotate', () => {
 
 test('crop', () => {
     const mesh = MeshBuilder.buildRectangle(
-        new Vertex(0,0,0),
-        new Vertex(10,0,0),
-        new Vertex(10,10,0),
-        new Vertex(0,10,0) );
+        new Vertex(0, 0, 0),
+        new Vertex(10, 0, 0),
+        new Vertex(10, 10, 0),
+        new Vertex(0, 10, 0)
+    );
     expect(mesh.triangles.length).toBe(2);
-    const scene1 = MeshOperations.crop(mesh, new BoundingBox(0,0,0,10,10,10));
+    const scene1 = MeshOperations.crop(
+        mesh,
+        new BoundingBox(0, 0, 0, 10, 10, 10)
+    );
     expect(scene1.meshes.length).toBe(2);
     expect(scene1.meshes[0].triangles.length).toBe(0);
     expect(scene1.meshes[1].triangles.length).toBe(2);
@@ -47,10 +55,11 @@ test('crop', () => {
 
 test('mirror', () => {
     const mesh = MeshBuilder.buildRectangle(
-        new Vertex(0,0,0),
-        new Vertex(10,0,0),
-        new Vertex(10,10,0),
-        new Vertex(0,10,0) );
+        new Vertex(0, 0, 0),
+        new Vertex(10, 0, 0),
+        new Vertex(10, 10, 0),
+        new Vertex(0, 10, 0)
+    );
     expect(mesh.triangles.length).toBe(2);
     const mesh2 = MeshOperations.mirror(mesh, true, false, false);
     expect(mesh2.triangles.length).toBe(2);
@@ -58,43 +67,47 @@ test('mirror', () => {
 
 test('translate', () => {
     const mesh = MeshBuilder.buildRectangle(
-        new Vertex(0,0,0),
-        new Vertex(10,0,0),
-        new Vertex(10,10,0),
-        new Vertex(0,10,0) );
+        new Vertex(0, 0, 0),
+        new Vertex(10, 0, 0),
+        new Vertex(10, 10, 0),
+        new Vertex(0, 10, 0)
+    );
     expect(mesh.triangles.length).toBe(2);
-    const mesh2 = MeshOperations.translate(mesh, new Vertex(20,30,40));
+    const mesh2 = MeshOperations.translate(mesh, new Vertex(20, 30, 40));
     expect(mesh2.triangles.length).toBe(2);
 });
 
 test('scale', () => {
     const mesh = MeshBuilder.buildRectangle(
-        new Vertex(0,0,0),
-        new Vertex(10,0,0),
-        new Vertex(10,10,0),
-        new Vertex(0,10,0) );
+        new Vertex(0, 0, 0),
+        new Vertex(10, 0, 0),
+        new Vertex(10, 10, 0),
+        new Vertex(0, 10, 0)
+    );
     expect(mesh.triangles.length).toBe(2);
-    const mesh2 = MeshOperations.scale(mesh, new Vertex(20,30,40));
+    const mesh2 = MeshOperations.scale(mesh, new Vertex(20, 30, 40));
     expect(mesh2.triangles.length).toBe(2);
 });
 
 test('scaleToSize', () => {
     const mesh = MeshBuilder.buildRectangle(
-        new Vertex(0,0,0),
-        new Vertex(10,0,0),
-        new Vertex(10,10,0),
-        new Vertex(0,10,0) );
+        new Vertex(0, 0, 0),
+        new Vertex(10, 0, 0),
+        new Vertex(10, 10, 0),
+        new Vertex(0, 10, 0)
+    );
     expect(mesh.triangles.length).toBe(2);
-    const mesh2 = MeshOperations.scaleToSize(mesh, "xyz", 40);
+    const mesh2 = MeshOperations.scaleToSize(mesh, 'xyz', 40);
     expect(mesh2.triangles.length).toBe(2);
 });
 
 test('flipNormals', () => {
     const mesh = MeshBuilder.buildRectangle(
-        new Vertex(0,0,0),
-        new Vertex(10,0,0),
-        new Vertex(10,10,0),
-        new Vertex(0,10,0) );
+        new Vertex(0, 0, 0),
+        new Vertex(10, 0, 0),
+        new Vertex(10, 10, 0),
+        new Vertex(0, 10, 0)
+    );
     expect(mesh.triangles.length).toBe(2);
     const mesh2 = MeshOperations.flipNormals(mesh);
     expect(mesh2.triangles.length).toBe(2);
@@ -102,23 +115,24 @@ test('flipNormals', () => {
 
 test('replicate', () => {
     const mesh = MeshBuilder.buildRectangle(
-        new Vertex(0,0,0),
-        new Vertex(10,0,0),
-        new Vertex(10,10,0),
-        new Vertex(0,10,0) );
+        new Vertex(0, 0, 0),
+        new Vertex(10, 0, 0),
+        new Vertex(10, 10, 0),
+        new Vertex(0, 10, 0)
+    );
     expect(mesh.triangles.length).toBe(2);
     const mesh2 = MeshOperations.replicate(mesh, 10, 5, 5, 100);
-    expect(mesh2.triangles.length).toBe(2*100);
+    expect(mesh2.triangles.length).toBe(2 * 100);
 });
 
 test('center', () => {
     const mesh = MeshBuilder.buildRectangle(
-        new Vertex(0,0,0),
-        new Vertex(10,0,0),
-        new Vertex(10,10,0),
-        new Vertex(0,10,0) );
+        new Vertex(0, 0, 0),
+        new Vertex(10, 0, 0),
+        new Vertex(10, 10, 0),
+        new Vertex(0, 10, 0)
+    );
     expect(mesh.triangles.length).toBe(2);
     const mesh2 = MeshOperations.center(mesh);
     expect(mesh2.triangles.length).toBe(2);
 });
-
